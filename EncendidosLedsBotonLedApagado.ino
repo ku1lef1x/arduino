b720bcb079422a83c010d394ef82c40a548c7426
ku1lef1x <47606966+ku1lef1x@users.noreply.github.com>
2019-02-14 21:09:18 +0100

    Add files via upload

---
+++ EncendidosLedsBotonLedApagado.ino
@@ -0,0 +1,27 @@
+int boton = 8;
+int led = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(boton,INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int estado = digitalRead(boton);
+
+  if (estado == LOW){
+
+    digitalWrite(led, HIGH);
+  }
+
+  if (estado == HIGH){
+    digitalWrite(led,LOW);
+  }
+
+}
